80c9aae56f4ce258bb770cf51578cfe794c0368f
KseniaMik <miksen95@mail.ru>
2015-10-23 20:17:34 +0300

    routs

--- WebApplication1/WebApplication1/App_Start/RouteConfig.cs
+++ WebApplication1/WebApplication1/App_Start/RouteConfig.cs
@@ -14,10 +14,31 @@ namespace WebApplication1
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
+                name: "Index",
+                url: "Index",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+
+            routes.MapRoute(
+               name: "Image",
+               url: "Image",
+               defaults: new { controller = "Home", action = "Image", id = UrlParameter.Optional }
+           );
+
+
+            routes.MapRoute(
+              name: "Music",
+              url: "Music",
+              defaults: new { controller = "Home", action = "Music", id = UrlParameter.Optional }
+          );
+
+            routes.MapRoute(
+              name: "History",
+              url: "History",
+              defaults: new { controller = "Home", action = "History", id = UrlParameter.Optional }
+          );
+
         }
     }
 }
