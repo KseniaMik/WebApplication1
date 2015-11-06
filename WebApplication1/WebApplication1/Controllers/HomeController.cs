74a1f903183ff84c89f05163c07995f30c05fde6
KseniaMik <miksen95@mail.ru>
2015-11-06 20:22:49 +0300

    06.11.2015

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -9,7 +9,10 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
- 
+        public ActionResult Index()
+        {
+            return View();
+        }
 
         public ActionResult Image()
         {
@@ -20,6 +23,37 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
+
+        public ActionResult Comment()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Comment(Comment comment)
+        {
+            var comments = Session["comments"] as List<Comment>;
+            if (comments == null)
+            {
+                comments = new List<Comment>();
+            }
+            comments.Add(comment);
+            Session["comments"] = comments;
+
+            return RedirectToAction("Comments");
+        }
+
+
+        public ActionResult Comments()
+        {
+            var comments = Session["comments"] as List<Comment>;
+            
+           
+          
+
+            return View(comments);
+        }
+
 
         public ActionResult Music()
         {
